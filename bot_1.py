7c9144e2977f17cb77e851c81996d76539146295
donald1218 <donald.shih2000@gmail.com>
2024-03-04 22:59:49 +0800

    modified key from issue title to issue number and fixed bug

--- bot_1.py
+++ bot_1.py
@@ -5,7 +5,7 @@ import datetime
 import json
 
 intents = discord.Intents.all()
-bot = commands.Bot(command_prefix='!',intents = intents)
+bot = commands.Bot(command_prefix='%',intents = intents)
 
 
 
@@ -14,8 +14,11 @@ DC_token = ""
 channel_ID = 0
 repo_name =""
 
-manager= []
+managers= []
 issue_with_user = {}
+with open("issue.json") as f:
+    issue_with_user = json.load(f)
+
 
 with open("secrets.json") as f:
     data = json.load(f)
@@ -35,13 +38,27 @@ async def on_ready():
     check_github_for_updates.start()
     check_github_for_new.start()
     check_github_for_closed.start()
+    with open("data.json") as f:
+        manager = json.load(f)
+        channel = bot.get_channel(channel_ID)
+        if channel:
+            users = channel.members
+            for user in users :
+                if(user.name in manager):
+                    managers.append(user)
 
 @bot.command()
 async def add_manager(ctx, user: discord.Member):
     channel = bot.get_channel(channel_ID)
+
     if channel:
-        if user not in manager:
-            manager.append(user)
+        if user not in managers:
+            managers.append(user)
+            with open("data.json") as f:
+                names = json.load(f)
+                names.append(user.name)
+            with open("data.json","w") as f:
+                json.dump(names,f,indent=3)
             await channel.send(f'{user.display_name} has been recorded as an administrator.')
         else:
             await channel.send(f'{user.display_name}  is already an administrator.')
@@ -50,60 +67,80 @@ async def add_manager(ctx, user: discord.Member):
 async def remove_manager(ctx, user: discord.Member):
     channel = bot.get_channel(channel_ID)
     if channel:
-        if user not in manager:
+        if user not in managers:
             await channel.send(f'{user.display_name} is not an administrator.')
         else:
-            manager.remove(user)
+            managers.remove(user)
             await channel.send(f'{user.display_name}  was removed from administrators.')        
 
 @bot.command()
 async def assign(ctx, user: discord.Member,*, message: str):
-    issue_with_user[message]=user
+    issue_with_user[message]=user.name
     channel = bot.get_channel(channel_ID)
     if channel:
-        await channel.send(f'{message} is assigned to {issue_with_user[message].mention}')   
+        await channel.send(f'issue #{message} is assigned to {user.mention}')
+        with open("issue.json","w") as f:
+            json.dump(issue_with_user,f,indent=3)
 
 @tasks.loop(time=datetime.time(hour = 4,minute=30))
 async def check_github_for_new():
     channel = bot.get_channel(channel_ID)
     yesterday = datetime.datetime.now(tz=datetime.timezone.utc) - datetime.timedelta(days=1)
-    if manager:
+    if managers:
         try:
             repo = user.get_repo(repo_name)
             issues = repo.get_issues(state='open',since=yesterday)
             for issue in issues:
                 if(issue.created_at>yesterday):
-                    message = ', '.join([admin.mention for admin in manager])
-                    await channel.send(f'{message} new issue : '+ issue.title)
+                    message = ', '.join([admin.mention for admin in managers])
+                    await channel.send(f'{message} new issue : #'+ str(issue.number))
                     await channel.send(issue.html_url)
                     
         except Exception as e:
             print(f'Error occurred while checking GitHub for updates: {e}')
 
 
-@tasks.loop(time=datetime.time(hour = 4,minute=31))  
+@tasks.loop(time=datetime.time(hour = 4,minute=31)) 
 async def check_github_for_updates():
     yesterday = datetime.datetime.now(tz=datetime.timezone.utc) - datetime.timedelta(days=1)
+    td = datetime.datetime.now(tz=datetime.timezone.utc) - datetime.timedelta(minutes=1)
     channel = bot.get_channel(channel_ID)
     if channel:
         try:
             repo = user.get_repo(repo_name)
             issues = repo.get_issues(state='open',since=yesterday)
             for issue in issues:
-                if(issue.created_at<yesterday):
-                    if(issue.title in issue_with_user):
-                        await channel.send(f'{issue_with_user[issue.title].mention} '+issue.title+' is update ')
+                if(issue.created_at<td):
+                    for key in issue_with_user:
+                        if(int(key) == issue.number):
+                            users = channel.members
+                            for member in users:
+                                if(member.name == issue_with_user[str(issue.number)]):
+                                    await channel.send(f'{member.mention} issue #'+str(issue.number)+' is update ')
+                            await channel.send(issue.html_url)
+                            break
         except Exception as e:
             print(f'Error occurred while checking GitHub for updates: {e}')
 
-@tasks.loop(time=datetime.time(hour = 4,minute=0))  
+@tasks.loop(time=datetime.time(hour = 4,minute=0)) 
 async def check_github_for_closed():
     try:
         repo = user.get_repo(repo_name)
         issues = repo.get_issues(state='open')
-        for issue in issue_with_user:
-                if(issue not in issues):
-                    del issue_with_user[issue]
+        delete = []
+        for issue_num in issue_with_user:
+                isOpen = False
+                for issue in issues:
+                    if(issue.number==int(issue_num)):
+                        isOpen = True
+                        break
+                if(isOpen==False):
+                    delete.append(issue_num)
+
+        for num in delete:
+            del issue_with_user[num]
+        with open("issue.json","w") as f:
+            json.dump(issue_with_user,f,indent=3)
 
     except Exception as e:
         print(f'Error occurred while checking GitHub for updates: {e}')
--- bot_2.py
+++ bot_2.py
@@ -7,12 +7,6 @@ import json
 DC_token = ""
 channel_ID = 0
 
-with open("secrets.json") as f:
-    data = json.load(f)
-    DC_token = data['dc-token']
-    channel_ID = data['channel_ID']
-
-
 # intents是要求機器人的權限
 intents = discord.Intents.all()
 # command_prefix是前綴符號，可以自由選擇($, #, &...)
@@ -20,36 +14,79 @@ bot = commands.Bot(command_prefix = "%", intents = intents)
 
 issue_with_user = {}
 
+
+with open("secrets.json") as f:
+    data = json.load(f)
+    DC_token = data['dc-token']
+    channel_ID = data['channel_ID']
+
+manager = []
+
+
+
+
+
+# with open("data.json") as f:
+#     data = json.load(f)
+#     manager = data['users']
+
+
+
+
+
+
 @bot.event
 # 當機器人完成啟動
 async def on_ready():
     print(f"目前登入身份 --> {bot.user}")
     test1.start()
     test2.start()
+    with open("data.json") as f:
+        managers = json.load(f)
+        channel = bot.get_channel(channel_ID)
+        if channel:
+            users = channel.members
+            for user in users :
+                if(user.name in managers):
+                    manager.append(user)
 
 @bot.command()
 # 輸入%Hello呼叫指令
 async def setting(ctx, user: discord.Member):
     # 替换 CHANNEL_ID 为你想要标记的频道 ID
-    global channel_ID
     channel = bot.get_channel(channel_ID)
     if channel:
         await channel.send(f'{user.mention}, 你被标记了！')
 
 @bot.command()
+async def add_manager(ctx, user: discord.Member):
+    channel = bot.get_channel(channel_ID)
+    if channel:
+        if user not in manager:
+            manager.append(user)
+            with open("data.json") as f:
+                names = json.load(f)
+                names.append(user.name)
+            with open("data.json","w") as f:
+                json.dump(names,f,indent=3)
+            await channel.send(f'{user.display_name} has been recorded as an administrator.')
+        else:
+            await channel.send(f'{user.display_name}  is already an administrator.')
+
+@bot.command()
 async def assign(ctx, user: discord.Member,*, message: str):
+    channel = bot.get_channel(channel_ID)
     # 替换 CHANNEL_ID 为你想要标记的频道 ID
     issue_with_user[message]=user
-    channel = bot.get_channel(channel_ID)
     print(issue_with_user)
     if channel:
         await channel.send(f' {issue_with_user[message].mention}')   
 
 @bot.command()
 async def remove(ctx, *, message: str):
+    channel = bot.get_channel(channel_ID)
     # 替换 CHANNEL_ID 为你想要标记的频道 ID
     del issue_with_user[message]
-    channel = bot.get_channel(channel_ID)
     print(issue_with_user)
     if channel:
         await channel.send(f' {issue_with_user}')   
